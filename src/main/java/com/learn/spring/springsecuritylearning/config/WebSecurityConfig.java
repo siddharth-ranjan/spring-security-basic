d981bbf92f9edecfb1d4607b7e9be061d68bbd15
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2024-01-17 15:18:21 +0530

    before 2nd day

--- src/main/java/com/learn/spring/springsecuritylearning/config/WebSecurityConfig.java
+++ src/main/java/com/learn/spring/springsecuritylearning/config/WebSecurityConfig.java
@@ -1,11 +1,13 @@
 package com.learn.spring.springsecuritylearning.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@Configuration
 public class WebSecurityConfig {
 
     @Bean
--- src/main/java/com/learn/spring/springsecuritylearning/controller/RegistrationController.java
+++ src/main/java/com/learn/spring/springsecuritylearning/controller/RegistrationController.java
@@ -4,9 +4,7 @@ import com.learn.spring.springsecuritylearning.entity.UserEntity;
 import com.learn.spring.springsecuritylearning.model.UserModel;
 import com.learn.spring.springsecuritylearning.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RegistrationController {
